c58e574c3fa0592189a022f663964d14c2be939c
Rubby Jane <rdoclara@gmail.com>
2022-06-05 08:26:26 +0800

    save

--- website/jquery.js
+++ website/jquery.js
@@ -11,71 +11,14 @@ $(document).ready(function () {
   $("#moon").click(function () {
     $("#moon").hide();
     $("#sun").show();
-    $("form button, form input, .topnav-right a, h1,h2, #sun, #tablink, .fa").css("color", "#bdc1c6");
-    $(".tablinks active").css("color", "black");
+    $("form button, form input, .topnav-right a, h1,h2, #sun, .fa").css("color", "#bdc1c6");
     $("p, .logo").css("color", "#E1D9D1");
     $(".wrapper, .blog_post").css("background", "#141e24");
     $(".topnav, .header, .footer").css("background", "#1c1c1c");
 
-    $(document).ready(function () {
-      $("#active").click(function () {
-        $("#tablink").css("color", "white");
-        $(this).css("color", "black");
 
-        $(document).ready(function () {
-          $("#tablink").mouseenter(function () {
-            $("#tablink").css("color", "black");
-          });
-        });
 
-        $(document).ready(function () {
-          $("#tablink").mouseleave(function () {
-            $("#tablink").css("color", "white");
-          });
-        });
 
-        $(document).ready(function () {
-          $("#active").mouseleave(function () {
-            $("#active").css("color", "black");
-          });
-        });
-      });
-    });
-
-    $(document).ready(function () {
-      $("#tablink").click(function () {
-        $("#active").css("color", "white");
-
-        $(document).ready(function () {
-          $("#active").mouseenter(function () {
-            $("#active").css("color", "black");
-          });
-        });
-
-        $(document).ready(function () {
-          $("#active").mouseleave(function () {
-            $("#active").css("color", "white");
-          });
-        });
-
-        $(document).ready(function () {
-          $("#tablink").mouseleave(function () {
-            $("#tablink").css("color", "black");
-          });
-        });
-      });
-    });
-    $(document).ready(function () {
-      $("#tablink").mouseenter(function () {
-        $("#tablink").css("color", "black");
-      });
-    });
-
-    $(document).ready(function () {
-      $("#tablink").mouseleave(function () {
-        $("#tablink").css("color", "white");
-      });
-    });
   });
 });
 
@@ -83,7 +26,7 @@ $(document).ready(function () {
   $("#sun").click(function () {
     $("#sun").hide();
     $("#moon").show();
-    $(" form button, .tab button,  form input, p").css("color", "#333");
+    $(" form button,   form input, p").css("color", "#333");
     $(".fa, .fa-moon").css("color", "#c38d9e");
     $(".fa-grip, .fa-square-pen, .fa-user, .fa-envelope-open-text").css("color", "#553d67");
     $(".logo,.topnav-right a,h1, h2").css("color", "#183153");
